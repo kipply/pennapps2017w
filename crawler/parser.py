82bf0215e104c35aea9ec86f9db0c8576e130ad6
Jack Sarick <jacksarick@gmail.com>
2017-01-20 23:01:12 -0500

    Function to rip content from wattpad

---
+++ crawler/parser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import re
+from html import unescape
+
+# Full wattpad site -> [list of strings]
+def rip_content(raw_file):
+  # Convert file to list of ugly strings
+  pattern = "<p data-p-id=\".{32}\">([^\n]+)</p>"
+  strings = re.findall(pattern, raw_file)
+
+  strip = lambda s: unescape(re.sub('</?[^\>]+>', '', s))
+  return "\n".join([strip(line) for line in strings])
+
+test = open(input("> ")[0:-1]).read()
+print(rip_content(test))
