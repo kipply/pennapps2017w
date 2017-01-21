a628372cc30a489cab73fd3fa4df57aa1c06e839
Jack Sarick <jacksarick@gmail.com>
2017-01-21 03:27:23 -0500

    Save Sat 21 Jan 2017 03:27:23 EST

--- crawler/loader.py
+++ crawler/loader.py
@@ -10,4 +10,4 @@ with open("teen-fic.txt", "w+") as output:
 	for link in link_list:
 		counter += 1
 		print(str(counter) + "/" + str(len(link_list)))
-		f.write(urlopen(Request(link, headers={'User-Agent': 'H4X0R/6.6.6'})).read())
+		output.write(urlopen(Request(link, headers={'User-Agent': 'H4X0R/6.6.6'})).read())
