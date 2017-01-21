9880e884106bd9efdd5f9f5a8b37e59fe7d66de0
Jack Sarick <jacksarick@gmail.com>
2017-01-21 15:27:31 -0500

    Save Sat 21 Jan 2017 15:27:31 EST

--- run.py
+++ run.py
@@ -4,10 +4,4 @@ from app import app
 from flask import Flask
 
 if __name__ == '__main__': # If it is being run directly, otherwise __name__ would be the name of the module
-  try:
-    context = ()
-    print("Running HTTPS")
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context) 
-  except Exception as e:
-    print("Running HTTP")
-    app.run(host='0.0.0.0', port=80, debug=True)
+  app.run(host='0.0.0.0', port=80, debug=True) 
