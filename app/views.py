93500bd2d455e3b69b5705e81bf05d84bc62dc46
Carol Chen <carolyfisher@gmail.com>
2017-01-21 02:13:37 -0500

    Commiting because Jack said so

--- app/views.py
+++ app/views.py
@@ -2,7 +2,10 @@ from app import app
 from flask import render_template, redirect, request, url_for
 from htmlmin.minify import html_minify
 
+from .forms import CreateBook
+
 @app.route('/')
 def index(): 
   # Do something 
-  return html_minify(render_template('index.html'))
+  form = CreateBook()
+  return html_minify(render_template('index.html', form = form))
