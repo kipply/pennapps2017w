ef73624f2fc3c87b711610683c4180a11b5408af
Jack Sarick <jacksarick@gmail.com>
2017-01-21 03:24:43 -0500

    Curtis had the genius idea of a loading bar

--- crawler/loader.py
+++ crawler/loader.py
@@ -4,5 +4,10 @@ from urllib.request import Request, urlopen
 
 id_list = open(input("ids.txt")[0:-1]).read()[0:-1].split(",")
 link_list = ["https://www.wattpad.com/" + c_id for c_id in id_list]
-for link in link_list:
-	print(urlopen(Request(link, headers={'User-Agent': 'H4X0R/6.6.6'})).read())
+
+counter = 0
+with open("teen-fic.txt", "w+") as output:
+	for link in link_list:
+		counter += 1
+		print(counter+"/"+len(link_list))
+		f.write(urlopen(Request(link, headers={'User-Agent': 'H4X0R/6.6.6'})).read())
