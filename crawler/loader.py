ad0a7e6e1e214dd5ae6e31da1167129b09b75802
Jack Sarick <jacksarick@gmail.com>
2017-01-21 03:05:11 -0500

    No mas manual

--- crawler/loader.py
+++ crawler/loader.py
@@ -2,7 +2,7 @@
 
 from urllib.request import Request, urlopen
 
-id_list = open(input("> ")[0:-1]).read()[0:-1].split(",")
+id_list = open(input("ids.txt")[0:-1]).read()[0:-1].split(",")
 link_list = ["https://www.wattpad.com/" + c_id for c_id in id_list]
 for link in link_list:
 	print(urlopen(Request(link, headers={'User-Agent': 'H4X0R/6.6.6'})).read())
