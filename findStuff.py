0ff744f1fc14625f2f3c98eeb8fa5b889b26110b
jacksarick <jacksarick@gmail.com>
2017-01-22 13:17:42 +0000

    idiot broke it within seconds. We're patching it now

--- findStuff.py
+++ findStuff.py
@@ -1,37 +1,38 @@
 from writePDF import writePDF
 import requests
-import json 
+import json
 import sys
 from generate_story import generate
-  
-def findStuff(message): 
 
-  results = requests.get("http://api.meaningcloud.com/topics-2.0", 
+def findStuff(message):
+  message = "a " + message
+  message = message[:6666]
+  results = requests.get("http://api.meaningcloud.com/topics-2.0",
               params={'key': '4e9d9d2ab61341074dd513e4422aa47e', 'of': 'json', 'lang': 'en', 'ilang': 'en', 'txt': message, 'tt': 'a'})
   prompts = []
-  for concept in results.json()['concept_list']: 
-    if concept['relevance'] == '100': 
+  for concept in results.json()['concept_list']:
+    if concept['relevance'] == '100':
       prompts.append(concept['form'])
 
-  for entity in results.json()['entity_list']: 
-    if entity['relevance'] == '100': 
+  for entity in results.json()['entity_list']:
+    if entity['relevance'] == '100':
       prompts.append(entity['form'])
-  if len(prompts) < 7: 
+  if len(prompts) < 7:
     bigPrompt = ""
-    for prompt in prompts: 
+    for prompt in prompts:
       bigPrompt += prompt + " "
-    prompts.append(bigPrompt) 
+    prompts.append(bigPrompt)
 
   stories = []
-  if len(prompts) > 2: 
-    for prompt in prompts: 
+  if len(prompts) > 2:
+    for prompt in prompts:
       stories.append(generate(prompt))
       print(stories)
-  else: 
-    while len(prompts) < 2: 
+  else:
+    while len(prompts) < 2:
       prompts = prompts * 2
 
-    for prompt in prompts: 
+    for prompt in prompts:
       print(stories)
       stories.append(generate(prompt))
   print(stories)
