b5878bd40e76125f16455286a821a3c2ca351dc2
Carol Chen <carolyfisher@gmail.com>
2017-01-21 22:00:21 -0500

    WOW

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from flask import Flask
 
 if __name__ == '__main__': # If it is being run directly, otherwise __name__ would be the name of the module
   context = ('server.crt', 'server.key')
-  app.run(host='0.0.0.0', port=443, ssl_context=context, debug=True) 
+  # app.run(host='0.0.0.0', port=443, ssl_context=context, debug=True) 
   app.run(debug=True) 
